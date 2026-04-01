66b5d100679cf446832dd1f8f98ca620f020753e
Maat <maat@hermes.prime>
2026-04-01 16:58:00 +0300

    Add fresh 1024x1024 blog images for 14 articles - Maat

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+export const runtime = 'nodejs'
+
 export async function middleware(request: NextRequest) {
   return NextResponse.next()
 }
