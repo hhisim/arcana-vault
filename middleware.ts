84bacdf1cf15632b7f3b6e2b5c74983d354b6425
Maat <maat@vaultofarcana.com>
2026-04-01 12:38:46 +0300

    fix: add missing NextResponse import in stripped middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   return NextResponse.next()
